8e60a21170d2cb1a79b9830ac79356cf2077893e
Alix Chagué <alix.chague@inria.fr>
2020-02-03 13:38:17 +0100

    change of process we know dynamically modify the list of names as the scrit is executed

--- memoguerrabot.py
+++ memoguerrabot.py
@@ -8,14 +8,34 @@
 import time
 import requests # use pip install requests
 import tweepy # use pip install tweepy
-from data import EXTRACTED_NAMES
+#from data import EXTRACTED_NAMES
+
+
+def load_data() -> str or bool:
+    """open a csv file, pick up the first line and return its content and update the csv"""
+    with open("data.csv", "r") as fh:
+        data = fh.read()
+    data = data.split("\n")
+    data = [] if data == [""] else data
+    if len(data) > 1:
+        wr = "\n".join(data[1:])
+        with open("data.csv", "w") as fh:
+            fh.write(wr)
+        return data[0]
+    elif len(data) == 1:
+        with open("data.csv", "w") as fh:
+            fh.write("")
+        return data[0]
+    else:
+        print("[NO MORE DATA]")
+        return False
 
 
 def create_message(name:str) -> str:
 	"""build a string (the content of the tweet), test its length (no more than 280) and return it"""
 	message = "{personne} fut l'une des victimes de la guerre d'Espagne et du franquisme.".format(personne=name)
 	#message = "{personne} - test".format(personne=name)
-	assert len(message) < 280,"TOO LONG ({}): '{}'".format(len(message), message)
+	assert len(message) < 280,"[TOO LONG] ({}): '{}'".format(len(message), message)
 	return message
 
 
@@ -39,11 +59,17 @@ def post_message(message: str) -> None:
 
 
 def main():
-	"""start a series of steps leading to the publication of a tweet on @memo_guerra_bot account"""
-	for name in EXTRACTED_NAMES:
-		message = create_message(name)
-		post_message(message)
-		time.sleep(3600) # will post a message every hour
+    """start a series of steps leading to the publication of a tweet on @memo_guerra_bot account"""
+    for i in range(25):
+        name = load_data()
+        if name:
+            message = create_message(name)
+            print("[TEST]: {}".format(message)) # for test
+            #post_message(message)
+            time.sleep(1)
+            #time.sleep(3600) # will post a message every hour
+        else:
+            print("`data.csv` is most likely empty.")
 
 
 if __name__ == '__main__':
